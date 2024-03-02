518726347e5548e2297cf61cdcf00810760eae9e
atakanwhite <161921062+atakanwhite@users.noreply.github.com>
2024-03-02 13:05:35 +0300

    Add files via upload

---
+++ python92 proje 1.py
@@ -0,0 +1,8 @@
+print("╔════════╔════════════════╗════════╗")
+print("║════════╚LIBRARY ARCHIVES╝════════║")
+print("║         1-  Famous Books         ║ ")
+print("║         2-  New York Best Sellers║ ")
+print("║         3-   Magazines           ║ ")                            
+print("║         4-   News                ║ ")
+print("╚══════════════════════════════════╝ ")
+
