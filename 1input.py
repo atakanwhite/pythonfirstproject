eaa2ddf7172e5cd4a2c07147f2f8397de85e1a85
atakanwhite <atakanakbaypro@gmail.com>
2024-03-10 10:14:38 +0300

    son proje duzenleme

--- 1input.py
+++
@@ -1,9 +0,0 @@
-##a = 5
-##b = 6
-##a = input()
-##b = input()
-a = int(input("1.sayıyı gir:"))
-b = int(input("2.sayıyı gir:"))
-print("Carpim:\t",a * b)
-
-
--- python92 proje 1.py
+++
@@ -1,8 +0,0 @@
-print("╔════════╔════════════════╗════════╗")
-print("║════════╚LIBRARY ARCHIVES╝════════║")
-print("║         1-  Famous Books         ║ ")
-print("║         2-  New York Best Sellers║ ")
-print("║         3-   Magazines           ║ ")                            
-print("║         4-   News                ║ ")
-print("╚══════════════════════════════════╝ ")
-
