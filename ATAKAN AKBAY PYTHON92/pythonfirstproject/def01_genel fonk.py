0821594fcfc4dcccfe463abd2423f1afeba9cee3
atakanwhite <atakanakbaypro@gmail.com>
2024-03-18 08:29:09 +0300

    def py

--- ATAKAN AKBAY PYTHON92/pythonfirstproject/def01_genel fonk.py
+++ ATAKAN AKBAY PYTHON92/pythonfirstproject/def01_genel fonk.py
@@ -1,8 +1,16 @@
-def topla():
-    print(f"Toplam:{3+7}")
+def topla(aa,bb):
+    print(f"Toplam:{aa+bb}")
 
-def carp():
-    print(f"Carpim:{3*7}")
+def carp(xx,yy):
+    print(f"Carpim:{xx*yy}")
 
-topla()
-carp()
+sayi1 = int(input("Birinci sayı nedir?"))
+sayi2 = int(input("İkinci sayı nedir?"))
+
+#sayi1 = 3
+#sayi2 = 9
+#topla(4,6)
+#topla(sayi1,sayi2)
+#topla(sayi1,topla(sayi2,sayi2))
+topla(sayi1,sayi2)
+carp(sayi1,sayi2)
