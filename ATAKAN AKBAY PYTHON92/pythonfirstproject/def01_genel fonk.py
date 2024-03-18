1e05b4666896f3c08ab247a850acb9a92c8943e7
atakanwhite <atakanakbaypro@gmail.com>
2024-03-18 08:46:17 +0300

    adasdasd

--- ATAKAN AKBAY PYTHON92/pythonfirstproject/def01_genel fonk.py
+++ ATAKAN AKBAY PYTHON92/pythonfirstproject/def01_genel fonk.py
@@ -1,8 +1,8 @@
 def topla(aa,bb):
-    print(f"Toplam:{aa+bb}")
+    return f"Toplam:{aa+bb}"
 
 def carp(xx,yy):
-    print(f"Carpim:{xx*yy}")
+    return f"Carpim:{xx*yy}"
 
 sayi1 = int(input("Birinci sayı nedir?"))
 sayi2 = int(input("İkinci sayı nedir?"))
@@ -12,5 +12,8 @@ sayi2 = int(input("İkinci sayı nedir?"))
 #topla(4,6)
 #topla(sayi1,sayi2)
 #topla(sayi1,topla(sayi2,sayi2))
-topla(sayi1,sayi2)
-carp(sayi1,sayi2)
+print (topla(sayi1,sayi2))
+print (carp(sayi1,sayi2))
+
+#yazdığımız fonk sayesinde f sonrasındaki değer sayı1,sayı2 yerine yazılmış oldu ve sonra returnle değer yerine döndü
+#fonksiyonlarda geri dönüş için return kullan 
