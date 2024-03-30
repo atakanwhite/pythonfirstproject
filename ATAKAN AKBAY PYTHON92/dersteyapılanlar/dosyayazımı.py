52b3d1708468e5e823036fb7fc2143d2c1e4067e
atakanwhite <atakanakbaypro@gmail.com>
2024-03-30 09:50:53 +0300

    adadsa

---
+++ ATAKAN AKBAY PYTHON92/dersteyapılanlar/dosyayazımı.py
@@ -0,0 +1,9 @@
+abc = open("dosyaornek.txt","w")
+
+abc.write("Dosyaya kaydedilen veri.\nİkinci satıra geçtik!!\n")
+
+abc.write("Üçüncü satır çalışmaya devam--")
+
+abc.write("!!!\n\n\n")
+
+abc.close()
