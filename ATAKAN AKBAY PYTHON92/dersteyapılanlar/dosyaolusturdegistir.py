98ca171655cf00a927f3d7ef9f6e853d5e7fe034
atakanwhite <atakanakbaypro@gmail.com>
2024-03-30 11:02:55 +0300

    readlime

--- ATAKAN AKBAY PYTHON92/dersteyapılanlar/dosyaolusturdegistir.py
+++ ATAKAN AKBAY PYTHON92/dersteyapılanlar/dosyaolusturdegistir.py
@@ -6,9 +6,20 @@
 # except :
 #     print("Bir hatayla karşılaşıldı.")
 
-not1=5
-with open ("sayılar.txt","a") as xx:
-    for a in range(10):
-        #xx.write(a) #dosyaya int yazılamaz
-        xx.write(f"{a}")
-        xx.write(f"\n{a}")
+# not1=5
+# with open ("sayılar.txt","a") as xx:
+#     for a in range(10):
+#         #xx.write(a) #dosyaya int yazılamaz
+#         xx.write(f"{a}")
+#         xx.write(f"\n{a}")
+
+try: 
+    okunan = open("dosyaornek.txt","r")
+    print(okunan.readline())
+    print(okunan.readline())
+    print(okunan.readline())
+    
+
+    okunan.close()
+except:
+    print("Bir hata oluştu.")
