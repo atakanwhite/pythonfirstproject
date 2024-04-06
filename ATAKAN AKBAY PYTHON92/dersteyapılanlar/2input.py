960480cf85bbba331bdc294f526050c43cd28b02
atakanwhite <atakanakbaypro@gmail.com>
2024-04-06 09:44:00 +0300

    asdasd

--- ATAKAN AKBAY PYTHON92/dersteyapılanlar/2input.py
+++ ATAKAN AKBAY PYTHON92/dersteyapılanlar/2input.py
@@ -5,4 +5,4 @@ yas=int(input(f"Hangi yılda doğdun {ad}\t"))
 print("{} demek {} yaşındasın".format(ad,2024-int(yas)))
 print(f"{ad} demek {2024-int(yas)} yaşındasın")
 print(ad,"demek",2024-int(yas),"yaşındasın")
-
+input()
