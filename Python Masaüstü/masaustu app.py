fba0aa3e4ccaabad4dc7390c4df4f06050344974
atakanwhite <atakanakbaypro@gmail.com>
2024-04-27 11:04:30 +0300

    pyqt5 masaustu app başlangıç

---
+++ Python Masaüstü/masaustu app.py
@@ -0,0 +1,7 @@
+from PyQt5.QtWidgets import *
+app = QApplication([])
+pencere = QWidget()
+pencere.show()
+app.exec()
+
+# 2 ve 3. satırdaki kodlar nesne sınıfı/// exec açılan tab in açık kalmasını sağlıyor.
